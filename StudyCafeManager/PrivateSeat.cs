646f7410f030b7015e718639b1896c2044844640
Emang <Emang@DESKTOP-DD3FU43>
2022-03-18 12:34:46 +0900

    프로젝트 뼈대

---
+++ StudyCafeManager/PrivateSeat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCafeManager
+{
+    class PrivateSeat : ISeat
+    {
+        private string seatnum;
+        public string SeatNum 
+        {
+            get { return seatnum; }
+        }
+        public PrivateSeat(string seatnum)
+        {
+            this.seatnum = seatnum;
+        }
+    }
+}
---
+++ StudyCafeManager/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCafeManager
+{
+    class Room : ISeat
+    {
+        private string seatnum;
+        public string SeatNum
+        {
+            get { return seatnum; }
+        }
+        public Room(string seatnum)
+        {
+            this.seatnum = seatnum;
+        }
+    }
+}
